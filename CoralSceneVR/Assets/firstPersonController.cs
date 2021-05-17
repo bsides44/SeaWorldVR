066af76ca89712f31166b935c25bf53bcc1c76ee
bsides44 <maddog.star@gmail.com>
2021-05-18 09:22:36 +1200

    added unity files and assets

---
+++ CoralSceneVR/Assets/firstPersonController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstPersonController : MonoBehaviour
+{
+    public Camera fPCamera;
+    float leftRight;
+    float inOut;
+    float upDown;
+    float rotLeftRight;
+    float rotUpDown;
+
+    void Update()
+    {
+        // float xAxisValue = Input.GetAxis("Horizontal");
+        // float zAxisValue = Input.GetAxis("Vertical");
+
+        //direction
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) { leftRight = leftRight - 1; };
+        if (Input.GetKeyUp(KeyCode.LeftArrow)) { leftRight = 0; };
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { leftRight = leftRight + 1; };
+        if (Input.GetKeyUp(KeyCode.RightArrow)) { leftRight = 0; };
+        if (Input.GetKeyDown(KeyCode.Space)) { inOut = inOut + 1; };
+        if (Input.GetKeyUp(KeyCode.Space)) { inOut = 0; };
+        if (Input.GetKeyDown(KeyCode.UpArrow)) { upDown = upDown + 1; };
+        if (Input.GetKeyUp(KeyCode.UpArrow)) { upDown = 0; };
+        if (Input.GetKeyDown(KeyCode.DownArrow)) { upDown = upDown - 1; };
+        if (Input.GetKeyUp(KeyCode.DownArrow)) { upDown = 0; };
+
+        // rotation
+        if (Input.GetKeyDown(KeyCode.A)) { rotLeftRight = rotLeftRight - 1; };
+        if (Input.GetKeyUp(KeyCode.A)) { rotLeftRight = 0; };
+        if (Input.GetKeyDown(KeyCode.D)) { rotLeftRight = rotLeftRight + 1; };
+        if (Input.GetKeyUp(KeyCode.D)) { rotLeftRight = 0; };
+        if (Input.GetKeyDown(KeyCode.W)) { rotUpDown = rotUpDown - 1; };
+        if (Input.GetKeyUp(KeyCode.W)) { rotUpDown = 0; };
+        if (Input.GetKeyDown(KeyCode.S)) { rotUpDown = rotUpDown + 1; };
+        if (Input.GetKeyUp(KeyCode.S)) { rotUpDown = 0; };
+
+        if (Camera.current != null)
+        {
+            Camera.current.transform.Translate(new Vector3(leftRight, upDown, inOut));
+            Camera.current.transform.Rotate(rotUpDown, rotLeftRight, 0, Space.Self);
+        }
+    }
+}
---
+++ CoralSceneVR/Assets/fishSchoolMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fishSchoolMovement : MonoBehaviour
+{
+    public GameObject fish;
+    float timer;
+    bool alt = true;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        fish.transform.Translate(new Vector3(0.1f, 0.0f, 0.0f));
+
+        if (timer >= 3 && alt)
+        {
+            timer = 0;
+            alt = !alt;
+            fish.transform.Rotate(0, 25, 0, Space.Self);
+        }
+        if (timer >= 3 && !alt)
+        {
+            timer = 0;
+            alt = !alt;
+            fish.transform.Rotate(0, 75, 0, Space.Self);
+        }
+
+    }
+}
